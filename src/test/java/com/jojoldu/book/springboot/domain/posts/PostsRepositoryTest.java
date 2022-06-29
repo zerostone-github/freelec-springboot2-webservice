49613bfcc7fbed4998bf0fc75711ebb73449abad
zerostone <zerostone@msn.com>
2022-06-29 23:09:26 +0900

    6/29 push

---
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+   @AfterEach
+    public  void cleanup() {
+       postsRepository.deleteAll();
+   }
+
+   @Test
+    public void 게시글저장_불러오기(){
+       String title = "테스트 게시글";
+       String content = "테스트 본문";
+
+       postsRepository.save(Posts.builder()
+               .title(title)
+               .content(content)
+               .author("jojoldu@gmail.com")
+               .build()
+       );
+
+       //When
+       List<Posts> postsList = postsRepository.findAll();
+
+       //then
+       Posts posts = postsList.get(0);
+       assertThat(posts.getTitle()).isEqualTo(title);
+       assertThat(posts.getContent()).isEqualTo(content);
+
+   }
+}
